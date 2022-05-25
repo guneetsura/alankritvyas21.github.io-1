a2feafc5141958c917f4384ebded39096aa4f8c2
Alankrit Vyas <66866845+alankritvyas21@users.noreply.github.com>
2022-05-25 12:34:43 +0530

    Update isotope.pkgd.js

--- assets/vendor/isotope-layout/isotope.pkgd.js
+++ assets/vendor/isotope-layout/isotope.pkgd.js
@@ -1,24 +1,4 @@
-/*!
- * Isotope PACKAGED v3.0.6
- *
- * Licensed GPLv3 for open source use
- * or Isotope Commercial License for commercial use
- *
- * https://isotope.metafizzy.co
- * Copyright 2010-2018 Metafizzy
- */
-
-/**
- * Bridget makes jQuery widgets
- * v2.0.1
- * MIT license
- */
-
-/* jshint browser: true, strict: true, undef: true, unused: true */
-
 ( function( window, factory ) {
-  // universal module definition
-  /*jshint strict: false */ /* globals define, module, require */
   if ( typeof define == 'function' && define.amd ) {
     // AMD
     define( 'jquery-bridget/jquery-bridget',[ 'jquery' ], function( jQuery ) {
@@ -53,7 +33,6 @@ var logError = typeof console == 'undefined' ? function() {} :
     console.error( message );
   };
 
-// ----- jQueryBridget ----- //
 
 function jQueryBridget( namespace, PluginClass, $ ) {
   $ = $ || jQuery || window.jQuery;
@@ -61,32 +40,23 @@ function jQueryBridget( namespace, PluginClass, $ ) {
     return;
   }
 
-  // add option method -> $().plugin('option', {...})
   if ( !PluginClass.prototype.option ) {
-    // option setter
     PluginClass.prototype.option = function( opts ) {
-      // bail out if not an object
       if ( !$.isPlainObject( opts ) ){
         return;
       }
       this.options = $.extend( true, this.options, opts );
     };
   }
-
-  // make jQuery plugin
   $.fn[ namespace ] = function( arg0 /*, arg1 */ ) {
     if ( typeof arg0 == 'string' ) {
-      // method call $().plugin( 'methodName', { options } )
-      // shift arguments by 1
       var args = arraySlice.call( arguments, 1 );
       return methodCall( this, arg0, args );
     }
-    // just $().plugin({ options })
     plainCall( this, arg0 );
     return this;
   };
 
-  // $().plugin('methodName')
   function methodCall( $elems, methodName, args ) {
     var returnValue;
     var pluginMethodStr = '$().' + namespace + '("' + methodName + '")';
@@ -106,9 +76,7 @@ function jQueryBridget( namespace, PluginClass, $ ) {
         return;
       }
 
-      // apply method, get return value
       var value = method.apply( instance, args );
-      // set return value if value is returned, use only first value
       returnValue = returnValue === undefined ? value : returnValue;
     });
 
@@ -119,11 +87,9 @@ function jQueryBridget( namespace, PluginClass, $ ) {
     $elems.each( function( i, elem ) {
       var instance = $.data( elem, namespace );
       if ( instance ) {
-        // set options & init
         instance.option( options );
         instance._init();
       } else {
-        // initialize new instance
         instance = new PluginClass( elem, options );
         $.data( elem, namespace, instance );
       }
@@ -133,10 +99,6 @@ function jQueryBridget( namespace, PluginClass, $ ) {
   updateJQuery( $ );
 
 }
-
-// ----- updateJQuery ----- //
-
-// set $.bridget for v1 backwards compatibility
 function updateJQuery( $ ) {
   if ( !$ || ( $ && $.bridget ) ) {
     return;
@@ -145,59 +107,35 @@ function updateJQuery( $ ) {
 }
 
 updateJQuery( jQuery || window.jQuery );
-
-// -----  ----- //
-
 return jQueryBridget;
 
 }));
 
-/**
- * EvEmitter v1.1.0
- * Lil' event emitter
- * MIT License
- */
-
-/* jshint unused: true, undef: true, strict: true */
 
 ( function( global, factory ) {
-  // universal module definition
-  /* jshint strict: false */ /* globals define, module, window */
   if ( typeof define == 'function' && define.amd ) {
-    // AMD - RequireJS
+  
     define( 'ev-emitter/ev-emitter',factory );
   } else if ( typeof module == 'object' && module.exports ) {
-    // CommonJS - Browserify, Webpack
+   
     module.exports = factory();
   } else {
-    // Browser globals
     global.EvEmitter = factory();
   }
-
 }( typeof window != 'undefined' ? window : this, function() {
-
-
-
 function EvEmitter() {}
-
 var proto = EvEmitter.prototype;
-
 proto.on = function( eventName, listener ) {
   if ( !eventName || !listener ) {
     return;
   }
-  // set events hash
   var events = this._events = this._events || {};
-  // set listeners array
   var listeners = events[ eventName ] = events[ eventName ] || [];
-  // only add once
   if ( listeners.indexOf( listener ) == -1 ) {
     listeners.push( listener );
   }
-
   return this;
 };
-
 proto.once = function( eventName, listener ) {
   if ( !eventName || !listener ) {
     return;
